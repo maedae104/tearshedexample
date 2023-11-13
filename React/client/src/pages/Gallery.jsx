8f8e712978c6c729a70187d789c0024efe403d3e
Mae Dae <MaeDae>
2023-11-13 09:07:26 -0800

    slideshow sizing

--- React/client/src/pages/Gallery.jsx
+++ React/client/src/pages/Gallery.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Fade } from "react-slideshow-image";
+import "../static/stylesheet.css"
 
 
 function Gallery() {
@@ -12,34 +13,55 @@ function Gallery() {
   
 
   return (
+
     <div>
       <h1 className="text-center">Gallery</h1>
       <div className="container">
-      <div className="slide-container">
-        <Fade>
-          <div className="each-fade">
-            <div className="image-container">
-              <img src={fadeImages[0]} alt="slide" />
-            </div>
-            <h2>First Slide</h2>
-          </div>
-          <div className="each-fade">
-            <div className="image-container">
-              <img src={fadeImages[1]} alt="slide" />
-            </div>
-            <h2>Second Slide</h2>
-          </div>
-          <div className="each-fade">
-            <div className="image-container">
-              <img src={fadeImages[2]} alt="slide" />
-            </div>
-            <h2>Third Slide</h2>
-          </div>
-        </Fade>
+        <div className="slide-container" >
+          <Fade>
+            {fadeImages.map((image, index) => (
+              <div className="each-fade" key={index}>
+                <div className="image-container">
+                  <img src={image} alt="slide" />
+                </div>
+                <h2>{`Slide ${index + 1}`}</h2>
+              </div>
+            ))}
+          </Fade>
+        </div>
       </div>
     </div>
-    </div>
+    
   );
 }
 
 export default Gallery;
+
+
+// <div>
+//       <h1 className="text-center">Gallery</h1>
+//       <div className="container">
+//       <div className="slide-container">
+//         <Fade>
+//           <div className="each-fade">
+//             <div className="image-container">
+//               <img src={fadeImages[0]} alt="slide" />
+//             </div>
+//             <h2>First Slide</h2>
+//           </div>
+//           <div className="each-fade">
+//             <div className="image-container">
+//               <img src={fadeImages[1]} alt="slide" />
+//             </div>
+//             <h2>Second Slide</h2>
+//           </div>
+//           <div className="each-fade">
+//             <div className="image-container">
+//               <img src={fadeImages[2]} alt="slide" />
+//             </div>
+//             <h2>Third Slide</h2>
+//           </div>
+//         </Fade>
+//       </div>
+//     </div>
+//     </div>
