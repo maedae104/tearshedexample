6c3ad14391af6a0d024b62be483d71c09022d17d
Mae Dae <MaeDae>
2024-01-17 15:54:09 -0800

    more changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals.js';
+// import reportWebVitals from './reportWebVitals.js';
 import AppRouter from './molecules/AppRouter.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -21,4 +21,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
