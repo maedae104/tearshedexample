e6645190675f2b4288e3eb1c2ec403b4ecb5b513
Mae Dae <MaeDae>
2024-01-17 16:30:16 -0800

    more change s

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App.jsx';
-// import reportWebVitals from './reportWebVitals.js';
-import AppRouter from './molecules/AppRouter.jsx';
+const React = require('react');
+const ReactDOM = require('react-dom/client');
+const { BrowserRouter: Router } = require('react-router-dom');
+const App = require('./App.jsx');
+const reportWebVitals = require('./reportWebVitals.js');
+const AppRouter = require('./molecules/AppRouter.jsx');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Router>
-  <AppRouter>
-  
-    <App />
-    
-    </AppRouter>
-</Router>
-
-
+      <App />
 );
+
+reportWebVitals();
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import App from './App.jsx';
+// // import reportWebVitals from './reportWebVitals.js';
+// import AppRouter from './molecules/AppRouter.jsx';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+// <Router>
+//   <AppRouter>
+  
+//     <App />
+    
+//     </AppRouter>
+// </Router>
+
+
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
