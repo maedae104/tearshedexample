51f58edef7f0944663b7875895fb9d6a87c036c4
Mae Dae <MaeDae>
2024-01-30 10:09:31 -0800

    navbar

--- src/atoms/NavMenu.jsx
+++ src/atoms/NavMenu.jsx
@@ -1,28 +1,136 @@
 import React from "react";
-import { Navbar, Nav, NavbarBrand } from "reactstrap";
-import { NavLink } from 'react-router-dom';
+import { styled, alpha } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Menu, { MenuProps } from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import Divider from '@mui/material/Divider';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-function NavMenu() {
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
+
+export function NavMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = anchorEl;
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <>
-      <div
-        style={{
-          display: "block",
-          width: 550,
-          padding: 30,
-        }}
+    <div>
+      <Button
+        id="demo-customized-button"
+        aria-controls={open ? 'demo-customized-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
       >
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">The Tear Shed Project</NavbarBrand>
-          <Nav className="mr-auto" navbar>
-            <NavLink to="/" activeClassName="active">Home</NavLink>
-            <NavLink to="/gallery" activeClassName="active">Gallery</NavLink>
-            <NavLink to="/about" activeClassName="active">About</NavLink>
-          </Nav>
-        </Navbar>
-      </div>
-    </>
+        Options
+      </Button>
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose} disableRipple>
+          <EditIcon />
+          Edit
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <FileCopyIcon />
+          Duplicate
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleClose} disableRipple>
+          <ArchiveIcon />
+          Archive
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <MoreHorizIcon />
+          More
+        </MenuItem>
+      </StyledMenu>
+    </div>
   );
 }
+export default NavMenu
 
-export default NavMenu;
+// import { Navbar, Nav, NavbarBrand } from "reactstrap";
+// import { NavLink } from 'react-router-dom';
+
+// function NavMenu() {
+//   return (
+//     <>
+//       <div
+//         style={{
+//           display: "block",
+//           width: 550,
+//           padding: 30,
+//         }}
+//       >
+//         <Navbar color="light" light expand="md">
+//           <NavbarBrand href="/">The Tear Shed Project</NavbarBrand>
+//           <Nav className="mr-auto" navbar>
+//             <NavLink to="/" activeClassName="active">Home</NavLink>
+//             <NavLink to="/gallery" activeClassName="active">Gallery</NavLink>
+//             <NavLink to="/about" activeClassName="active">About</NavLink>
+//           </Nav>
+//         </Navbar>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default NavMenu;
